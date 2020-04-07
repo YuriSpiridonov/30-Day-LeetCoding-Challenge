9aadef5c492ae483776e95b1b561d4bac7774374
Yuri <48239309+YuriSpiridonov@users.noreply.github.com>
2020-04-07 10:02:00 +0300

    Single Number
    Difficulty: Easy
    Submission Detail:
    16 / 16 test cases passed.
    Runtime: 7484 ms
    Memory: 16.3 MB

---
+++ Week 1/SingleNumber.py
@@ -0,0 +1,18 @@
+"""
+Task 1:
+    Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+    Note:
+    Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+"""
+
+
+class Solution:
+    #def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(nums):    
+        for num in nums:
+            if nums.count(num) == 1:
+                return num
+
+#Tests            
+array = [4,1,2,1,2]
+print(Solution.singleNumber(array))  
